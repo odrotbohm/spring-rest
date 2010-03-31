62ac8c914802001fff2bacd09b0acfee8aff8326
Oliver Gierke <info@olivergierke.de>
2010-03-31 11:08:08 +0200

    Apply correct formatting.

--- src/main/java/com/springsource/samples/WelcomeController.java
+++ src/main/java/com/springsource/samples/WelcomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 /**
  * Handles requests for the application welcome page.
  */
@@ -13,14 +14,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/welcome")
 public class WelcomeController {
 
-	private Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+    private Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
-	/**
-	 * Simply selects the welcome view to render by returning void and relying
-	 * on the default request-to-view-translator.
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public void welcome() {
-		logger.info("Welcome!");
-	}
+
+    /**
+     * Simply selects the welcome view to render by returning void and relying
+     * on the default request-to-view-translator.
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public void welcome() {
+
+        logger.info("Welcome!");
+    }
 }
